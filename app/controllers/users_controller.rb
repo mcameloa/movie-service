d217a782631dcc0d42c9b351ace711477e1bfa47
Milton Camelo <javicamelo@outlook.com>
2023-11-17 11:42:56 -0500

    change logic ok register controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: :created
+      render json: { token: TokenGenerator.generate_token(user) }, status: :ok
     else
       render json: user.errors, status: :unprocessable_entity
     end
--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -8,10 +8,16 @@ describe 'Registration API' do
       parameter name: :user, in: :body, schema: {
         type: :object,
         properties: {
-          username: { type: :string },
-          email: { type: :string },
-          password: { type: :string },
-          password_confirmation: { type: :string },
+          user: {
+            type: :object,
+            properties: {
+              username: { type: :string },
+              email: { type: :string },
+              password: { type: :string },
+              password_confirmation: { type: :string },
+            }
+          }
+
         },
         required: %w[username email password password_confirmation]
       }
@@ -25,7 +31,7 @@ describe 'Registration API' do
         }
       end
 
-      response(201, 'user created') do
+      response(200, 'user created') do
         let(:user) { { user: valid_attributes } }
         run_test!
       end
